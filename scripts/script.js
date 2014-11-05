0d10f1de3837c552bcbe9aeaf79f2eca2949de52
unknown <chaofeis@gmail.com>
2014-11-05 16:18:40 +0800

    big change

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,31 @@
 $(document).ready(function(){
 	$(".pull-me").click(function(){
-    $(".panel").slideToggle("slow");
+    $(".panel").slideToggle("fast");
   });
 
   $(".clearfix").mouseenter(function() {
        $(this).animate({
-           height: '+=10px'
+          height: '+=10px'
+        });
+       $("#right").mouseenter(function(){
+		      $(this).fadeTo("fast",1);
        });
-       $(".right",".left").mouseenter(function(){
-		$(this).fadeTo("fast",1);
-	});
-	$(".right",".left").mouseenter(function(){
-		$(this).fadeTo("fast",1);
-	});
-   });
+      $("#left").mouseenter(function(){
+          $(this).fadeTo("fast",1);
+       });     
+	/*$("#right","#left").mouseenter(function(){
+		$(this).fadeTo("fast",0);
+	});*/
+  });
    $(".clearfix").mouseleave(function() {
-       $(this).animate({
+      $(this).animate({
            height: '-=10px'
        }); 
-       $(".right",".left").mouseenter(function(){
-		$(this).fadeTo("fast",1);
-	});
-	$(".right",".left").mouseenter(function(){
-		$(this).fadeTo("fast",1);
-	});
-   });
-
-
-
+     $("#right").mouseleave(function(){
+		      $(this).fadeTo("fast",0);
+	     });
+	    $("#left").mouseleave(function(){
+		      $(this).fadeTo("fast",0);
+	     });     
+   }); 
 });
