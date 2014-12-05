a88e35619aee2ca0132c21638e4de27a5dfa0334
unknown <chaofeis@gmail.com>
2014-12-05 16:18:56 +0800

    add animation border-image and tab-change to save space

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-	$(".pull-me").click(function(){
+	/*首部下拉*/
+  $(".pull-me").click(function(){
     $(".panel").slideToggle("fast");
   });
-
+  /*左右动画及透明*/
   $(".clearfix").mouseenter(function() {
        $(this).animate({
           height: '+=10px'
@@ -29,5 +30,15 @@ $(document).ready(function(){
 	    $("#left").mouseleave(function(){
 		      $(this).fadeTo("fast",0.2);
 	     });     
-   }); 
+  }); 
+  /*左边标签页切换*/  
+    $("#css").css("display","block");
+    $("#left ol li").click(function(){
+        var a=$(this).index();
+        $("#left ol li").css("border-bottom","5px dotted #ccc");
+        $(this).css("border-bottom","none");
+        
+        $("#left ul").hide();
+        $("#left ul").eq(a).show();
+    });
 });
