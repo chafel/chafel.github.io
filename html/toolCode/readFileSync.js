66cc57e9997e56d93f75fdea4549b836e25a1339
chaofei <chaofeis@gmail.com>
2018-03-18 21:55:18 +0800

    remove all things

--- html/toolCode/readFileSync.js
+++
@@ -1,5 +0,0 @@
-var fs = require("fs");
-var buf = new Buffer(fs.readFileSync(process.argv[2]));
-var lnum = buf.toString().split("\n").length - 1;
-console.log(lnum);
-
--- html/toolCode/simpleHttp.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.get(process.argv[2],function(rs){
-    rs.setEncoding('utf8');
-    rs.on('data',console.log);
-    rs.on('error',console.error);
-});
-
